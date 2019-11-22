ae231e4d58c04d47381d4d7e5a20800455171b4f
Amanda Su <amandasu@dhcp-10-250-101-41.harvard.edu>
2019-11-22 18:29:15 -0500

    commit after finishing draft of shiny app

--- app/app.R
+++ app/app.R
@@ -5,80 +5,164 @@ library(ggalluvial)
 library(shiny)
 library(shinythemes)
 library(coefplot)
+library(plotly)
 
-ui <- fluidPage(theme = shinytheme("flatly"),
-                navbarPage("International Students in the United States",
-                           
-                           tabPanel("About the Project",
-                                    
-                                    p("This project looks to analyze how individuals employed by Harvard University donate their political dollars. I visualize in multiple different ways the size of the donations, the support different candidates recieve, and how political ideology impacts donations. I also look at how these donations differ within the graduate schools and how all of the donations compare to the general population's donations."),
-                                    p("I accessed data on the political donations of individuals employed by Harvard University on the website of the Federal Election Committee, which releases information on every donation made to a presidential, senate, or house political campaign, including the name of the donor, their occupation, their employer, the donated amount, their state, and the recipient campaign."),
-                                    p("Although this data was easily accessible by downloading a csv file, there was much data cleaning to be done to ensure that my analyses are accurate. First, I only observed donations made to 2020 presidential campaigns, excluding senate and house donations. Second, I filtered for donations made after January 1st, 2017, as there was no presidential donations to 2020 campaigns in 2016 or earlier. Third, I had to parse through the contributor employer data and filter out donations whose donors were not actually employed by Harvard University. For example, the donations from the employees of Harvard-Westlake School were present in the data, as they contain the word “Harvard.” Donations like this were removed."),
-                                    p("I also used the Federal Election Committee website to find data on the campaign donations of the general population, the total campaign sizes, and the political party affiliations of the different committees."),
-                                    p("Additionally, I sourced data from a DataForProgress.org article, which offered ideology scores for 2020 candidates who served in Congress. The specific data was not publicly availible, and I had to reach out to the article author to access it.")
-                           ),
-                           
-                           tabPanel("About the Author",
-                                    h1("Amanda Y. Su"),
-                                    p("I'm a sophomore at Harvard College studying History & Literature. I'm originally from the Bay Area. I'm interested in issues of race, ethnicity, immigration and education. I'm planning to pursue career in journalism.")
-                           ),
-                           
-                           tabPanel("Geographical Regions of Origin",
-                                    imageOutput("region")
-                           ),
-                           
-                           tabPanel("Indicators of Countries of Origin", 
-                                    p("Select from the following plots to see how employees of Harvard's graduate schools donate to 2020 presidential campaigns."),
-                                    selectInput("indicator", label = h5("Select Plot"), 
-                                                choices = list("Gross Domestic Product" = "gdp",
-                                                               "Freedom of Expression" = "free_expression",
-                                                               "Educational Equality" = "edu_equality"), 
-                                                selected = "gdp",
-                                                multiple = FALSE,
-                                                selectize = FALSE,
-                                                width = '400px',
-                                                size = 1),
-                                    imageOutput("country_indicator")
-                                    
-                           ),
-                           
-                           tabPanel("Sources of Funding",
-                                    
-                                    imageOutput("funding")
-                           )))
+country_indicators <- read_rds("app/country_indicators.rds")
+funding <- read_rds("app/funding.rds")
+
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
+  navbarPage(
+    "International Students in the United States",
+
+    tabPanel(
+      "About the Project",
+
+      p("This project looks to analyze various patterns in the experiences and motivations of international students in the United States. I visualize in several ways the distribution of international students across regions of origin from 2009 to 2019, the different indicators in countries of origin that may influence how many international students study in the U.S., and the distribution of international students’ primary sources of funding."),
+      p("I used data from the Institute of International Education, the World Bank, and Varieties of Democracy. The IIE dataset tracks the number of international students in the U.S. and their countries of origin throughout the last decade. The World Bank dataset has hundreds of statistics for different country indicators, including Gross Domestic Product and population. The Varieties of Democracy dataset measures democracy in different countries by hundreds of factors, including my variables of interest: education equality and freedom of academic expression."),
+      p("Though I could easily download all the data as Excel files, there was a significant amount of data cleaning that could occur before I could join my various datasets. The final IIE dataset required me to join two datasets for the years 2009-2014 and the years 2015-2019. I also had to clean the wbdata dataset by only selecting my two variables of interest — population and gross domestic product per capita, along with country code country name, and country year. For the vdem dataset, I selected only my two variables of interest — education equality and freedom of academic expression, along with country name, country code, and year. I also filtered for only observations past 2009 in the wbdata and vdem datasets since I only had IIE data past 2009. I then used the wbstats package to create a country code variable for the original IIE dataset so that I could easily join the three datasets together by country code since spelling and formatting for different country names varied drastically across datasets.")
+    ),
+
+
+    tabPanel(
+      "Geographical Regions of Origin",
+      plotlyOutput("region")
+    ),
+
+    tabPanel(
+      "Indicators of Countries of Origin",
+      p("Select from the following plots to see the correlation between various country of origin indicators and the number of international students in the U.S. from those countries"),
+      selectInput("indicator",
+        label = h5("Select Plot"),
+        choices = list(
+          "Gross Domestic Product Per Capita" = "gdp",
+          "Freedom of Expression" = "free_expression",
+          "Educational Equality" = "edu_equality"
+        ),
+        selected = "gdp",
+        multiple = FALSE,
+        selectize = FALSE,
+        width = "400px",
+        size = 1
+      ),
+      plotlyOutput("country_indicator")
+    ),
+
+    tabPanel(
+      "Sources of Funding",
+
+      plotlyOutput("funding")
+    ),
+    tabPanel(
+      "About the Author",
+      h1("Amanda Y. Su"),
+      p("I'm a sophomore at Harvard College studying History & Literature. I'm originally from the Bay Area. I'm interested in issues of race, ethnicity, immigration and education. I'm planning to pursue career in journalism.")
+    )
+  )
+)
 
 
 server <- function(input, output) {
-    
-    output$country_indicator <- renderImage({
-        
-        if(input$indicator == "gdp") {     
-            list(src = "gdp_map.gif",
-                 contentType = "image/gif")
-        } 
-        
-        else if(input$indicator == "free_expression") {
-            list(src = "free_expression_map.png",
-                 contentType = "image/png")
-        }
-        
-        else if(input$indicator == "edu_equality"){
-            list(src = "edu_equality_map.png",
-                 contentType = "image/png")
-        }
+  output$country_indicator <- renderPlotly({
+    if (input$indicator == "gdp") {
+      ggplotly(country_indicators %>%
+
+        ggplot(aes(x = gdp_per_capita, y = num_students, size = population)) +
+        geom_point() +
+        ylim(0, 30000) +
+        labs(
+          title = "Number of International Students Depending on Origin Country's GDP", subtitle = "Year: {frame_time}",
+          x = "Gross Domestic Product Per Capita ($)", y = "Number of Students", size = "Population"
+        ) +
+        scale_size_continuous(labels = comma))
     }
-    )
-    
-    output$region <- renderImage({
-        list(src = "region_graph.png",
-             contentType = "image/png")
-    })
-    
-    output$funding <- renderImage({
-        list(src = "funding_map.png",
-             contentType = "image/png")
+
+    else if (input$indicator == "free_expression") {
+      ggplotly(country_indicators %>%
+        filter(year == "2017") %>%
+        ggplot(aes(x = freedom_expression, y = num_students, size = population)) +
+        ylim(0, 30000) +
+        geom_point(aes(colour = "red")) +
+        geom_smooth(method = "lm", se = F, colour = "gray") +
+        labs(
+          title = "Number of International Students Depending on Origin Country's Freedom of Expression",
+          subtitle = "Year: 2017", x = "Freedom of Expression", y = "Number of Students", population = "Population"
+        ) +
+        scale_size_continuous(labels = comma) +
+        scale_colour_discrete(guide = "none") +
+        theme(legend.position = "none"))
     }
-    )
+
+    else if (input$indicator == "edu_equality") {
+      ggplotly(country_indicators %>%
+        filter(year == "2017") %>%
+        ggplot(aes(x = education_equality, y = num_students, size = population)) +
+        ylim(0, 30000) +
+        geom_point() +
+        geom_smooth(method = "lm", se = F, colour = "gray") +
+        labs(
+          title = "Number of International Students Depending on Origin Country's Educational Equality",
+          subtitle = "Year: 2017", x = "Education Equality", y = "Number of Students", population = "Population"
+        ) +
+        scale_size_continuous(labels = comma) +
+        scale_colour_discrete(guide = "none"))
+    }
+  })
+
+  output$region <- renderPlotly({
+    ggplotly(country_indicators %>%
+      filter(!is.na(population)) %>%
+      filter(!is.na(region)) %>%
+      group_by(region, year) %>%
+      summarize(prop_students = sum(num_students) / sum(population)) %>%
+      ggplot(aes(x = year, y = prop_students, fill = year)) +
+      geom_col() +
+      facet_wrap(~region) +
+      theme(
+        strip.text = element_text(size = 7),
+        axis.text.x = element_text(angle = 90, size = 5),
+        axis.text.y = element_text(size = 5),
+        legend.position = "none"
+      ) +
+      scale_y_continuous(labels = function(x) format(x, scientific = FALSE)) +
+      labs(
+        title = "Proportion of Origin Region's Population that are International Students in the United States", x = "Year",
+        y = "Proportion"
+      ))
+  })
+
+  output$funding <- renderPlotly({
+    ggplotly(funding %>%
+      filter(funding_source != "Total Students" &
+        funding_source != "International Funding Sources" &
+        funding_source != "U.S. Funding Sources") %>%
+      mutate(funding_source = fct_relevel(
+        funding_source,
+        c(
+          "U.S. Government",
+          "U.S. Private Sponsor",
+          "U.S. College or University",
+          "Foreign Private Sponsor",
+          "Foreign Government or University",
+          "International Organization",
+          "Current Employment",
+          "Personal and Family",
+          "Other Sources"
+        )
+      )) %>%
+      ggplot(aes(x = funding_source, y = num_students, fill = funding_source)) +
+      geom_col() +
+      theme(
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()
+      ) +
+      labs(
+        title = "Primary Sources of Funding for International Students in the U.S.", subtitle = "2017-2018 School Year",
+        x = "Funding Source", fill = "Funding Source", y = "Number of International Students in the U.S."
+      ) +
+      scale_y_continuous(labels = function(x) format(x, scientific = FALSE)))
+  })
 }
 
 
--- app/prep.shiny.r
+++ app/prep.shiny.r
@@ -4,27 +4,13 @@ library(gganimate)
 library(tidyverse)
 library(save.r)
 
-file_copy("graphics/region_graph.rds", "app/region_graph.rds")
+country_indicators <- read_rds("clean-data/country_indicators.rds")
+write_rds(country_indicators, "app/country_indicators.rds")
 
-file_copy("graphics/gdp_map.rds", "app/gdp_map.rds")
+funding <- read_rds("clean-data/funding.rds")
+write_rds(funding, "app/funding.rds")
 
-file_copy("graphics/free_expression_map.rds", "app/free_expression_map.rds")
 
-file_copy("graphics/edu_equality_map.rds", "app/edu_equality_map.rds")
-
-file_copy("graphics/funding_map.rds", "app/funding_map.rds")
-
-region_map <- readRDS("app/region_graph.rds")
-gdp_map <- readRDS("app/gdp_map.rds")
-free_expression_map <- readRDS("app/free_expression_map.rds")
-edu_equality_map <- readRDS("app/edu_equality_map.rds")
-funding_map <- readRDS("app/funding_map.rds")
-
-ggsave("app/region_graph.png", region_map)
-anim_save("app/gdp_map.gif")
-ggsave("app/free_expression_map.png", free_expression_map)
-ggsave("app/edu_equality_map.png", edu_equality_map)
-ggsave("app/funding_map.png", funding_map)
 
 
 
